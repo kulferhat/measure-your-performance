a4e04a1b65b61cb19411e5543d6642c8bd25d21b
ferhatk <ferhatk@telenity.com>
2014-12-05 18:48:41 +0200

    with dependencies

---
+++ dist/run.bat
@@ -0,0 +1,2 @@
+export CLASSPATH=commons-lang-2.6.jar;commons-io-2.4.jar;myp.jar;JNativeHook.jar:%CLASSPATH
+java kul.ferhat.MeasureYourPerformance
---
+++ dist/run.sh
@@ -0,0 +1,3 @@
+export CLASSPATH=commons-lang-2.6.jar:commons-io-2.4.jar:myp.jar:JNativeHook.jar:$CLASSPATH
+java kul.ferhat.MeasureYourPerformance
+
