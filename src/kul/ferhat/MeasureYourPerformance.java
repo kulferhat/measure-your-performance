e672c09b6441ca987a7131203187f00d7f94886e
ferhatk <ferhatk@telenity.com>
2014-12-05 18:37:31 +0200

    test commit

--- src/kul/ferhat/MeasureYourPerformance.java
+++ src/kul/ferhat/MeasureYourPerformance.java
@@ -29,7 +29,6 @@ public class MeasureYourPerformance {
         catch (NativeHookException ex) {
             System.err.println("There was a problem registering the native hook.");
             System.err.println(ex.getMessage());
-
             System.exit(1);
         }
 		
