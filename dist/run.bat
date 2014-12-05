8460d6787d1d73598c66ff1719e75d0f560404c6
ferhatk <ferhatk@telenity.com>
2014-12-05 19:04:47 +0200

    fix

--- dist/run.bat
+++ dist/run.bat
@@ -1,2 +1,2 @@
-export CLASSPATH=commons-lang-2.6.jar;commons-io-2.4.jar;myp.jar;JNativeHook.jar:%CLASSPATH
+set CLASSPATH=commons-lang-2.6.jar;commons-io-2.4.jar;myp.jar;JNativeHook.jar;%CLASSPATH%
 java kul.ferhat.MeasureYourPerformance
